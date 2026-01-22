82df19f8ba13b0ce36a86bca6c559f6d5b53393d
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2026-01-21 21:20:31 -0800

    Updates

--- src/player/bunny.gd
+++ src/player/bunny.gd
@@ -18,6 +18,13 @@ func _exit_tree() -> void:
 func _ready() -> void:
     super._ready()
 
+    if G.network.is_client:
+        G.network.local_authority_added.connect(
+            _on_local_authority_added,
+            CONNECT_ONE_SHOT,
+        )
+    _set_up_camera.call_deferred()
+
 
 func _process_movement_and_actions() -> void:
     super._process_movement_and_actions()
@@ -30,6 +37,28 @@ func play_sound(sound_name: String) -> void:
             pass
         "land":
             pass
+
+
+func _on_local_authority_added(
+        _input_from_client: PlayerInputFromClient,
+) -> void:
+    _set_up_camera()
+
+
+func _set_up_camera() -> void:
+    var is_local_player := multiplayer_id == G.network.local_id
+
+    G.print(
+        "Setting up camera for player %d (local=%d, is_local=%s)" % [
+            multiplayer_id,
+            G.network.local_id,
+            is_local_player,
+        ],
+        ScaffolderLog.CATEGORY_CORE_SYSTEMS,
+        ScaffolderLog.Verbosity.VERBOSE,
+    )
+
+    %CharacterCamera.enabled = is_local_player
 
 
 func get_string() -> String:
