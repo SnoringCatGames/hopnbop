b3bb603aa1e9ac8e9bf2b9363812334bc904acc2
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2026-01-23 15:09:01 -0800

    Updates

--- test/helpers/test_settings.gd
+++ test/helpers/test_settings.gd
@@ -1,10 +1,9 @@
 class_name TestSettings
-extends "res://src/core/settings.gd"
+extends Settings
 
 # Minimal settings configuration for CI testing
 
 func _init():
-    super._init()
     # Override any settings that might cause issues in CI
     preview_connect_to_remote_server = false
     preview_run_multiple_clients = false
