00e6aeddb49c5e98b9fc057e24eda1fa42ac8b71
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2026-01-27 14:07:12 -0800

    Updates

--- gamelift-gdextension/gamelift-server-sdk/cmake/External_rapidjson.cmake
+++ gamelift-gdextension/gamelift-server-sdk/cmake/External_rapidjson.cmake
@@ -7,14 +7,8 @@ ExternalProject_Add( rapidjson
     PREFIX "rapidjson"
     GIT_REPOSITORY "https://github.com/Tencent/rapidjson.git"
     GIT_TAG v1.1.0
-    PATCH_COMMAND
-        ${CMAKE_COMMAND} -E echo "Patching rapidjson CMakeLists.txt for modern CMake" && \
-        cd ${CMAKE_CURRENT_BINARY_DIR}/rapidjson/src/rapidjson && \
-        sed -i.bak -E 's/cmake_minimum_required\(VERSION ([0-9]+(\.[0-9]+)?)\)/cmake_minimum_required(VERSION 3.5...3.30)/g' CMakeLists.txt && \
-        sed -i.bak -E 's/CMAKE_MINIMUM_REQUIRED\(VERSION ([0-9]+(\.[0-9]+)?)\)/CMAKE_MINIMUM_REQUIRED(VERSION 3.5...3.30)/g' CMakeLists.txt
     UPDATE_COMMAND ""
     BUILD_COMMAND ""
-    -DCMAKE_POLICY_VERSION_MINIMUM=3.5
     CMAKE_ARGS
         -DRAPIDJSON_BUILD_TESTS=OFF
         -DRAPIDJSON_BUILD_DOC=OFF
