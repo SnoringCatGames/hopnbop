c0a3ea2ff816b26faf4f9df8f1c0d1f14b6170fc
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2026-02-04 15:23:08 -0800

    Updates

---
+++ addons/rollback_netcode/plugin.gd
@@ -0,0 +1,12 @@
+@tool
+extends EditorPlugin
+
+
+func _enter_tree() -> void:
+	# Plugin initialization (future: add custom nodes, inspectors, etc.)
+	pass
+
+
+func _exit_tree() -> void:
+	# Plugin cleanup
+	pass
